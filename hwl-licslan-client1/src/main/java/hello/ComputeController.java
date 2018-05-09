31f59596e757c207177c99287b2fa14e585a6c91
hwl <licslan@sina.com>
2018-05-09 17:48:11 +0800

    first commit

---
+++ hwl-licslan-client1/src/main/java/hello/ComputeController.java
@@ -0,0 +1,37 @@
+package hello;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ComputeController {
+
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    public final static Logger logger = LoggerFactory.getLogger(ComputeController.class);
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        List<ServiceInstance> instanceList = client.getInstances(applicationName);
+        ServiceInstance instance = instanceList.get(0);
+        Integer r = a + b;
+        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return r;
+    }
+
+}
